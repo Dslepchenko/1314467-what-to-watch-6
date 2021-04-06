6c508c5de406a8f0b89d3f45212874141bb868f1
Dslepchenko <dslepchenko@gmail.com>
2021-04-06 20:29:55 +0300

    исправил ковычки

--- src/components/Tabs/Movie_Review.jsx
+++ src/components/Tabs/Movie_Review.jsx
@@ -5,7 +5,7 @@ const MoviesReview = () =>
     <div className="movie-card__reviews-col">
       <div className="review">
         <blockquote className="review__quote">
-          <p className="review__text">Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.</p>
+          <p className="review__text">Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director`&quot;`rsquos funniest and most exquisitely designed movies in years.</p>
           <footer className="review__details">
             <cite className="review__author">Kate Muir</cite>
             <time className="review__date" dateTime="2016-12-24">December 24, 2016</time>
@@ -15,7 +15,7 @@ const MoviesReview = () =>
       </div>
       <div className="review">
         <blockquote className="review__quote">
-          <p className="review__text">Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.</p>
+          <p className="review__text">Anderson`&quot;`rsquos films are too precious for some, but for those of us willing to lose ourselves in them, they`&quot;`rsquore a delight. `&quot;`The Grand Budapest Hotel`&quot;` is no different, except that he has added a hint of gravitas to the mix, improving the recipe.</p>
           <footer className="review__details">
             <cite className="review__author">Bill Goodykoontz</cite>
             <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
@@ -25,7 +25,7 @@ const MoviesReview = () =>
       </div>
       <div className="review">
         <blockquote className="review__quote">
-          <p className="review__text">I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.</p>
+          <p className="review__text">I didn`&quot;`t find it amusing, and while I can appreciate the creativity, it`&quot;`s an hour and 40 minutes I wish I could take back.</p>
           <footer className="review__details">
             <cite className="review__author">Amanda Greever</cite>
             <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
